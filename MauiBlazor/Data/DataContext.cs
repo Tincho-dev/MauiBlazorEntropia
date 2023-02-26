0d40511539dd3a02c4f5531d067690c7e9baaa9f
Martin Lopez Rubio <Martin.LopezRubio@alu.frt.utn.edu.ar>
2023-02-26 17:09:58 -0300

    Add project files.

---
+++ MauiBlazor/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Fuente>().HasData(
+                new Fuente { CadenaFuente = "Esta es una fuente ya en memoria" },
+                new Fuente { CadenaFuente = "Este texto representa el libro del quijote de la mancha" }
+            );
+        }
+        public DbSet<Fuente> Fuentes => Set<Fuente>();
+    }
+}
---
+++ MauiBlazor/MauiProgram.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using Data;
+using Microsoft.EntityFrameworkCore;
+using Radzen;
+using Services;
+
+namespace MauiBlazor;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		builder.Services.AddMauiBlazorWebView();
+		#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+
+        builder.Services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("videogamesdb"));
+        builder.Services.AddScoped<IFuenteService, FuenteService>();
+        builder.Services.AddScoped<DialogService>();
+        builder.Services.AddScoped<NotificationService>();
+        builder.Services.AddScoped<TooltipService>();
+        builder.Services.AddScoped<ContextMenuService>();
+
+
+        return builder.Build();
+	}
+}
